9b4958fe7f139341b06fda0efeb3fd9cd6a886e7
Victor Ribeiro <vvictorrib@gmail.com>
2023-06-02 16:08:08 -0300

    added key prop to lastregister

--- src/app/(private)/home/_components/LastRegisters.js
+++ src/app/(private)/home/_components/LastRegisters.js
@@ -7,8 +7,11 @@ export default function LastRegisters() {
     <div className="h-full flex flex-col">
       <p>Últimos 5 registros</p>
       <div className="my-2 flex-grow overflow-y-auto">
-        {results.map(() => (
-          <div className="p-10 my-2 bg-white w-full flex items-center justify-between cursor-pointer">
+        {results.map((item) => (
+          <div
+            key={item}
+            className="p-10 my-2 bg-white w-full flex items-center justify-between cursor-pointer"
+          >
             <p>mercado</p>
             <p>R$55,55</p>
           </div>
